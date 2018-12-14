6af8eefaded873363a5d784cab1caae7083c2334
gssantiiago <gs1.santiago@gmail.com>
2018-12-14 09:34:11 -0200

    alterações das rotas

--- config/routes.php
+++ config/routes.php
@@ -62,12 +62,12 @@ Router::scope('/', function (RouteBuilder $routes) {
      * its action called 'display', and we pass a param to select the view file
      * to use (in this case, src/Template/Pages/home.ctp)...
      */
-    $routes->connect('/', ['controller' => 'Pages', 'action' => 'display', 'home']);
+    $routes->connect('/', ['controller' => 'Users', 'action' => 'login']);
 
     /**
      * ...and connect the rest of 'Pages' controller's URLs.
      */
-    $routes->connect('/pages/*', ['controller' => 'Pages', 'action' => 'display']);
+    $routes->connect('/users/*', ['controller' => 'Users', 'action' => 'login']);
 
     /**
      * Connect catchall routes for all controllers.
